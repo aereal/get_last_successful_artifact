15e9cffd3da65ec4994a3613b001cae8d9c5b901
aereal <aereal@aereal.org>
2017-02-01 18:56:08 +0900

    bundle gem

---
+++ lib/get_last_successful_artifact.rb
@@ -0,0 +1,5 @@
+require "get_last_successful_artifact/version"
+
+module GetLastSuccessfulArtifact
+  # Your code goes here...
+end
---
+++ lib/get_last_successful_artifact/version.rb
@@ -0,0 +1,3 @@
+module GetLastSuccessfulArtifact
+  VERSION = "0.1.0"
+end
